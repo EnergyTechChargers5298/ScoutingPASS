f4ab1b4e8dcf292784b8d8977f3f396ce0ae3346
MalachiPatrick0718 <95603368+MalachiPatrick0718@users.noreply.github.com>
2022-03-19 23:03:50 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data =`
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2020ilch",
+        "defaultValue":"NYLI1",
         "required":"true"
       },
       "Match Level":{
