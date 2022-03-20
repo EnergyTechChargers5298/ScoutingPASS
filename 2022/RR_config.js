19959ee973d9920903983e188d1d5185b132b2eb
MalachiPatrick0718 <95603368+MalachiPatrick0718@users.noreply.github.com>
2022-03-19 23:08:35 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data =`
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"NYLI",
+        "defaultValue":"NYSU",
         "required":"true"
       },
       "Match Level":{
